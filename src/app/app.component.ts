a60d38720fed8d3880ccc1a3d45f5826aa3654b3
MikeStork <44142875+MikeStork@users.noreply.github.com>
2021-05-05 23:59:32 +0200

    update meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,11 @@ export class AppComponent {
   ngOnInit(){
     this.metaService.addTags([
       {name: 'keywords', content: 'MixBocian, Mix, Bocian, Kuchnie, Alno, Alnokuchnie, Miele, Kitchenaid, Meble kuchenne, AGD, Bosch, Siemens, BSH, Opole, Kuchnie opole, Liebherr, Burger, kuchnie burger, Bauformat, kuchnie bauformat, Impuls  '},
-      {name: 'description', content: 'Angular Universal Example'},
-      {name: 'robots', content: 'index, follow'}
+      {name: 'description', content: 'MixBocian to firma profesjonalnie realizująca dobór, projekt i montaż kuchni, sprzętu kuchennego oraz AGD. W swojej ofercie mamy: Alno, Bauformat, Burger, Kuhlmann, Miele, KitchenAid, Bosch, Siemens, Gaggenau, Nivona, Franke, Faber, Villeroy&amp;Boch i wiele więcej'},
+      {name: 'robots', content: 'index, follow'},
+      { name: 'author', content: 'Michał Bocian' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: 'UTF-8' }
     ]);
     document.querySelector(".nudge").addEventListener('mouseenter', ()=>{
       document.querySelector(".fb-drawer").classList.add('fbxpnd');
